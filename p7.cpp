25ed4bb725319b9b0c8babdaa64c85e027c0b5ea
prasagit99 <saumya20011124@gmail.com>
2024-10-20 21:08:20 +0530

    Add files via upload

---
+++ p7.cpp
@@ -0,0 +1,37 @@
+# include <iostream>
+using namespace std;
+int main ()
+{
+    cout << "Enter the number " << endl;
+    int a;
+    cin >> a;
+    cout << "Number is " << a;
+    cout << endl;
+
+    cout << "Enter the Name " << endl;
+    string b ;
+    cin >> b;
+    cout << "Name is " << b;
+    cout << endl;
+
+    cout << "Enter the number " << endl;
+    float c;
+    cin >> c;
+    cout << "Number is " << c;
+    cout << endl;
+
+    cout << "Enter the number " << endl;
+    double d;
+    cin >> d;
+    cout <<"Number is " << d;
+    cout << endl;
+
+    cout << "Enter the character " << endl;
+    char e;
+    cin >> e;
+    cout << "Character is " << e;
+    cout << endl;
+
+    return 0;
+}
+
